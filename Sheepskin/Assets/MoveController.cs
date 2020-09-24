bd665ecb9e14136065a0e02dda854781c94f7bee
Aramcho <osipyan.aram2017@ya.ru>
2020-09-25 00:04:51 +0300

    Woolf joystick control. Joystick from asset store(https://assetstore.unity.com/packages/tools/input-management/ujoystick-49186)

---
+++ Sheepskin/Assets/MoveController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private bl_Joystick Joystick;
+
+    [SerializeField] private float Speed = 5;
+
+    void Update()
+    {
+       
+        float v = Joystick.Vertical; //get the vertical value of joystick
+        float h = Joystick.Horizontal;//get the horizontal value of joystick
+
+        
+        Vector3 translate = (new Vector3(h, v, 0) * Time.deltaTime) * Speed;
+        transform.Translate(translate);
+    }
+}
