1bd5159c93cac9c133412cde11f90967579a42ec
Aramcho <osipyan.aram2017@ya.ru>
2020-09-26 01:12:40 +0300

    setted logic of movement with animation

--- Sheepskin/Assets/MoveController.cs
+++ Sheepskin/Assets/MoveController.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class MoveController : MonoBehaviour
 {
     [SerializeField] private bl_Joystick Joystick;
 
     [SerializeField] private float Speed = 5;
+    Animator _animator;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
 
     void Update()
     {
@@ -14,8 +22,26 @@ public class MoveController : MonoBehaviour
         float v = Joystick.Vertical; //get the vertical value of joystick
         float h = Joystick.Horizontal;//get the horizontal value of joystick
 
+        if (v==0 && h == 0)
+        {
+            Stop();
+        }
+        else
+        {
+            Walk();
+            Vector3 translate = (new Vector3(h, v, 0) * Time.deltaTime) * Speed;
+            transform.Translate(translate);
+        }
         
-        Vector3 translate = (new Vector3(h, v, 0) * Time.deltaTime) * Speed;
-        transform.Translate(translate);
+    }
+
+    private void Walk()
+    {
+        _animator.SetTrigger("go");
+    }
+
+    private void Stop()
+    {
+        _animator.SetTrigger("not_go");
     }
 }
