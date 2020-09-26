3d805f655e8b6047aae44ee4c9e75fa97d4244de
Aramcho <osipyan.aram2017@ya.ru>
2020-09-26 23:12:06 +0300

    the wolf turns in the direction where it goes

--- Sheepskin/Assets/MoveController.cs
+++ Sheepskin/Assets/MoveController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define DEBUG_LOG
+using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +12,7 @@ public class MoveController : MonoBehaviour
 
     [SerializeField] private float Speed = 5;
     Animator _animator;
+    [SerializeField] GameObject HeroSprite; 
     private void Start()
     {
         _animator = GetComponent<Animator>();
@@ -28,7 +31,17 @@ public class MoveController : MonoBehaviour
         else
         {
             Walk();
-            Vector3 translate = (new Vector3(h, v, 0) * Time.deltaTime) * Speed;
+            
+            Vector3 translate = (new Vector3(h, v, 0) * Time.deltaTime) * Speed;            
+            Vector3 lookPos = new Vector3(h,v,0);
+            
+            float angle = Mathf.Acos((new Vector3(h, v, 0)).normalized.x) * Mathf.Rad2Deg;
+            int rotateSign = - Math.Sign(Vector3.Cross(Vector3.forward, lookPos).x);
+            HeroSprite.transform.rotation = Quaternion.AngleAxis(rotateSign*angle - 90 , Vector3.forward); 
+#if DEBUG_LOG
+            Debug.DrawRay(transform.position, lookPos);
+            
+#endif
             transform.Translate(translate);
         }
         
