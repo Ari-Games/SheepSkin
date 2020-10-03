7dea8b9c32f03df109bd3e35df8b887b592ef9fb
Alisher <fazilov.99@mail.ru>
2020-10-03 16:42:17 +0300

    Added task manager

--- Sheepskin/Assets/Scripts/DayNight/DayNightManager.cs
+++ Sheepskin/Assets/Scripts/DayNight/DayNightManager.cs
@@ -6,10 +6,11 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class DayNightManager : MonoBehaviour
 {
     [SerializeField] private Light2D sun;
+    [SerializeField] private GameObject allLightsOnScene;
     [SerializeField] private float secondsInFullDay = 60f;
     [SerializeField] private float currentTimeOfDay = 0f;
-    [SerializeField] private float deltaTimeInDay;
-    [SerializeField] private float secondsInDay;
+    [SerializeField] private float deltaIntensityOfSun;
+    [SerializeField] private float deltaIntensityOfAllLights;
 
     private const float BEGIN_SUN_INTENSITY = 1f;
     private const float END_SUN_INTENSITY = 0.35f;
@@ -20,11 +21,14 @@ public class DayNightManager : MonoBehaviour
 
     private IEnumerator ChangingTimeOfDay()
     {
-        deltaTimeInDay = Mathf.Abs(END_SUN_INTENSITY - BEGIN_SUN_INTENSITY) / secondsInFullDay;
+        deltaIntensityOfSun = Mathf.Abs(END_SUN_INTENSITY - BEGIN_SUN_INTENSITY) / secondsInFullDay;
+        deltaIntensityOfAllLights = 1 / secondsInFullDay;
         sun.intensity = BEGIN_SUN_INTENSITY;
         for (currentTimeOfDay = 0f; currentTimeOfDay < secondsInFullDay; currentTimeOfDay++)
         {
-            sun.intensity -= deltaTimeInDay;
+            sun.intensity -= deltaIntensityOfSun;
+            foreach (var light in allLightsOnScene.GetComponentsInChildren<Light2D>())
+                light.intensity += deltaIntensityOfAllLights;
             yield return new WaitForSeconds(1f);
         }
     }
