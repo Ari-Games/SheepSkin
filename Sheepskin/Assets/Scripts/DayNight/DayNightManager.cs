fe0f128bc08491e55ed6d860a7b673d3b0978dee
Alisher <fazilov.99@mail.ru>
2020-10-03 12:28:29 +0300

    Added sprites, added night/day manager

---
+++ Sheepskin/Assets/Scripts/DayNight/DayNightManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class DayNightManager : MonoBehaviour
+{
+    [SerializeField] private Light2D sun;
+    [SerializeField] private float secondsInFullDay = 60f;
+    [SerializeField] private float currentTimeOfDay = 0f;
+    [SerializeField] private float deltaTimeInDay;
+    [SerializeField] private float secondsInDay;
+
+    private const float BEGIN_SUN_INTENSITY = 1f;
+    private const float END_SUN_INTENSITY = 0.35f;
+
+
+
+    private void Start() => StartCoroutine(ChangingTimeOfDay());
+
+    private IEnumerator ChangingTimeOfDay()
+    {
+        deltaTimeInDay = Mathf.Abs(END_SUN_INTENSITY - BEGIN_SUN_INTENSITY) / secondsInFullDay;
+        sun.intensity = BEGIN_SUN_INTENSITY;
+        for (currentTimeOfDay = 0f; currentTimeOfDay < secondsInFullDay; currentTimeOfDay++)
+        {
+            sun.intensity -= deltaTimeInDay;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
