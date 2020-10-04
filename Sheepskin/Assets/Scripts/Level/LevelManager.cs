3e7c5eabb035db0b598b712add47cbb29860fb9e
Alisher <fazilov.99@mail.ru>
2020-10-04 20:19:24 +0300

    Added final scene, update main menu, level1

--- Sheepskin/Assets/Scripts/Level/LevelManager.cs
+++ Sheepskin/Assets/Scripts/Level/LevelManager.cs
@@ -3,22 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
+using Flocking;
 
 namespace Assets.Scripts.Level
 {
     public class LevelManager : MonoBehaviour
     {
-        [SerializeField] public List<Task> tasks;
-        [SerializeField] public int countSheeps;
+        [SerializeField] private List<Task> tasks;
+        [SerializeField] private int countSheeps;
 
         [SerializeField] private Text timeOfDayText;
         [SerializeField] private Text countSheepsText;
         [SerializeField] private int startHour = 10;
         [SerializeField] private int countGameHour = 27;
 
+        [SerializeField] private Image panelTime;
+        [SerializeField] private Image panelSheeps;
+        [SerializeField] private Sprite nightTime;
+        [SerializeField] private Sprite nightSheeps;
+        [SerializeField] private FlockController flockController;
+
         private void Start()
         {
-            countSheepsText.text = countSheeps.ToString();
+            countSheepsText.text = flockController.flockSize.ToString();
             foreach (var task in tasks)
                 Debug.Log(task);
             StartCoroutine(ChangingOfTime());
@@ -31,6 +38,11 @@ namespace Assets.Scripts.Level
             for (int currentTimeInSeconds = 0; currentTimeInSeconds < DayNightManager.secondsInFullDay; currentTimeInSeconds++)
             {
                 int countHours = (int)TimeSpan.FromSeconds(currentTime).TotalHours;
+                if (startHour + countHours >= 17)
+                {
+                    panelSheeps.GetComponent<Image>().sprite = nightSheeps;
+                    panelTime.sprite = nightTime;
+                }
                 if (startHour + countHours >= 24){
                     currentTime = 0;
                     startHour = 0;
---
+++ Sheepskin/Assets/Scripts/TestBezier.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    enum States
+    {
+        Task1,
+        Task2,
+        Win,
+    }
+
+    public class TestBezier : MonoBehaviour
+    {
+
+        public Transform P0;
+        public Transform P1;
+        public Transform P2;
+        public Transform P3;
+
+        [Range(0, 1)]
+        public float t;
+
+        public IEnumerator Move()
+        {
+            while (t < 1)
+            {
+                transform.position = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, t);
+                t += 0.025f;
+                yield return new WaitForSeconds(0.05f);
+            }
+            transform.localScale = new Vector3(-1, 1, 1);
+            while (t > 0)
+            {
+                transform.position = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, t);
+                t -= 0.025f;
+                yield return new WaitForSeconds(0.05f);
+            }
+            transform.localScale = new Vector3(1, 1, 1);
+            StartCoroutine(Move());
+            yield return null;
+        }
+
+        void Start()
+        {
+            StartCoroutine(Move());
+        }
+
+
+        private void OnDrawGizmos()
+        {
+
+            int sigmentsNumber = 20;
+            Vector3 preveousePoint = P0.position;
+
+            for (int i = 0; i < sigmentsNumber + 1; i++)
+            {
+                float paremeter = (float)i / sigmentsNumber;
+                Vector3 point = Bezier.GetPoint(P0.position, P1.position, P2.position, P3.position, paremeter);
+                Gizmos.DrawLine(preveousePoint, point);
+                preveousePoint = point;
+            }
+
+        }
+    }
+}
